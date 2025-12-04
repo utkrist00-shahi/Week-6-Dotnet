90728133142c0ac7f2506611361531e9dd2f2d6a
utkrist00-shahi <utkristshahi56@gmail.com>
2025-12-04 10:24:06 +0545

    task 1

---
+++ Week6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Rectangle rectangle = new Rectangle();
+            rectangle.Length = 7.0;
+            rectangle.Breadth = 8.0;
+            Console.WriteLine(rectangle.ShowDetails);
+            Console.WriteLine($"Perimeter of a Rectangle: {rectangle.CalculatePerimeter}");
+            Console.WriteLine($"Area of a Rectangle: {rectangle.GetArea}");
+        }
+    }
+}
---
+++ Week6/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+
+        public double GetArea => length * breadth;
+
+        public string ShowDetails => $"Length: {length}, Breadth: {breadth}, Area: {GetArea}";
+
+
+        public double CalculatePerimeter => 2 * (length + breadth);
+
+    }
+}
